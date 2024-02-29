60ba495c31cd245bd8342dc1d1b48293d0c4cecd
Andrea12346578 <84756325+Andrea12346578@users.noreply.github.com>
2024-02-29 01:41:05 +0000

    correcciones

--- ReservamosProject/src/app/weather-searcher/weather-searcher.component.ts
+++ ReservamosProject/src/app/weather-searcher/weather-searcher.component.ts
@@ -24,11 +24,11 @@ export class WeatherSearcherComponent implements OnInit{
   dates: any[] = [];
   maxDate: any;
   cities: any;
-  weather: any = [];
+  weather: any[] = [];
   ngOnInit()
   {
     var someDate = new Date();
-    var numberOfDaysToAdd = 5;
+    var numberOfDaysToAdd = 6;
     var result = someDate.setDate(someDate.getDate() + numberOfDaysToAdd);
     for(let i = 0; i < numberOfDaysToAdd; i++)
     {
@@ -53,14 +53,37 @@ export class WeatherSearcherComponent implements OnInit{
   onSelectedCity(city: any)
   {
     this.form.get('cityData')?.setValue(city);
-    this.weather = [];
   }
 
   search()
   {
     
     this.service.getWeather(this.form.value.cityData.lat,this.form.value.cityData.long).subscribe(data => {
-      this.weather = data.list.filter((item:any) => new Date(item.dt_txt) < this.maxDate);
+      //this.weather = data.list.filter((item:any) => new Date(item.dt_txt) < this.maxDate);
+      let items = []
+      console.log(this.dates)
+      console.log(data.list)
+      this.dates.forEach(date1 => {
+        //console.log("p", date1.getTime())
+        //console.log("p", date1)
+        let datePart = [
+          date1.getFullYear(),
+          date1.getMonth() + 1,
+          date1.getDate()
+        ].map((n, i) => n.toString().padStart(i === 2 ? 2 : 2, "0")).join("-");
+
+        console.log(datePart)
+        let arrayh = data.list.filter((item:any) => 
+        (item.dt_txt.split(' ')[0] == datePart))
+        if(arrayh.length > 0)
+        this.weather.push(
+          { date: date1,
+            values: arrayh
+          });
+
+      })
+
+      console.log(this.weather)
     })
   }
 
